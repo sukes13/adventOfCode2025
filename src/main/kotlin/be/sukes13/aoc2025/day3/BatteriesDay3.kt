5ee1acf5bd1e0e940d0ecbea89286bf66b1e3e7a
sukes13 <sir_jelle_wouters@hotmail.com>
2025-12-07 14:25:16 +0100

    Refactoring day 3

--- src/main/kotlin/be/sukes13/aoc2025/day3/BatteriesDay3.kt
+++ src/main/kotlin/be/sukes13/aoc2025/day3/BatteriesDay3.kt
@@ -2,46 +2,30 @@ package be.sukes13.aoc2025.day3
 
 import be.sukes13.aoc2025.mapLines
 
-fun part1(input: String) = input.toRacks().turnOnBatteries().sum()
+fun part1(input: String) = input.toRacks().sumOf { it.turnOnBatteries(2) }
 
-fun part2(input: String) = input.toRacks().sumOf { rack ->
-    rack.turnOnBatteries2(12)
-}
+fun part2(input: String) = input.toRacks().sumOf { it.turnOnBatteries(12) }
 
-fun List<Int>.turnOnBatteries2(numberOfBatteries: Int): Long {
-    return (numberOfBatteries downTo 1).fold(this to "") { (remainingRack,currentResult), remainingNumberOfBatteries ->
-        val (nextIndex, nextRack) = findNext(remainingRack, remainingNumberOfBatteries)
-        nextRack to currentResult + remainingRack[nextIndex]
+fun List<Int>.turnOnBatteries(numberOfBatteries: Int) =
+    (numberOfBatteries - 1 downTo 0).fold(this to "") { (remainingRack, currentResult), remainingNumberOfBatteries ->
+        val indexOfHighestUsable = remainingRack.indexOfHighestUsable(remainingNumberOfBatteries)
+        val newRack = remainingRack.drop(indexOfHighestUsable + 1)
+        val newResult = currentResult + remainingRack[indexOfHighestUsable]
+        newRack to newResult
     }.second.toLong()
-}
 
-private fun findNext(remainingRack: List<Int>, remainingNumberOfBatteries: Int): Pair<Int, List<Int>> {
-    val firstIndex = remainingRack.findHighestUsable2(remainingNumberOfBatteries)
-    val rackAfterFirst = remainingRack.drop(firstIndex + 1)
-    return firstIndex to rackAfterFirst
-}
-
-private fun List<Int>.findHighestUsable2(numberOfBatteries: Int) = run breaker@{
-    (0 until size - numberOfBatteries).fold(0) { acc, _ ->
-        val indexOfHighestUsable = findHighestUsable(numberOfBatteries - 1)
-        if (indexOfHighestUsable == -1) acc else return@breaker indexOfHighestUsable
+private fun List<Int>.indexOfHighestUsable(numberOfBatteries: Int) = run breaker@{
+    repeat(size - numberOfBatteries) {
+        val indexOfHighestUsable = indexOfHighestUsableOrNull(numberOfBatteries)
+        if (indexOfHighestUsable != null) return@breaker indexOfHighestUsable
     }
+    error("something went wrong")
 }
 
-private fun List<Int>.findHighestUsable(numberOfBatteries: Int): Int {
-    val indexOfHighest = indexOfFirst { it == maxOrNull() }
-    return if (indexOfHighest + numberOfBatteries >= size) {
-        indexOfFirst { it == dropLast(numberOfBatteries).maxOrNull() }
-    } else indexOfHighest
-}
-
-private fun List<List<Int>>.turnOnBatteries(): List<Int> {
-    return map { rack ->
-        val firstBatteryIndex = rack.findHighestUsable(1)
-        val secondBattery = rack.drop(firstBatteryIndex + 1).maxOrNull()
-        "${rack[firstBatteryIndex]}$secondBattery".toInt()
+private fun List<Int>.indexOfHighestUsableOrNull(numberOfBatteries: Int) =
+    indexOfFirst { it == dropLast(numberOfBatteries).maxOrNull() }.let { indexOfFirst ->
+        if (indexOfFirst == -1) null else indexOfFirst
     }
-}
 
 private fun String.toRacks() = mapLines { it.toRack() }
 

89a1c9c422b25a3f5ad281ae930964f83ee423d9
sukes13 <sir_jelle_wouters@hotmail.com>
2025-12-10 20:24:03 +0100

    WIP: day 7 part 2

--- src/main/kotlin/be/sukes13/aoc2025/day7/TachyonBeamDay7.kt
+++ src/main/kotlin/be/sukes13/aoc2025/day7/TachyonBeamDay7.kt
@@ -12,18 +12,36 @@ fun part2(input: String) = input.toManifold().sendQuantumBeam()
 data class Manifold(
     val splitters: List<Point>,
     val start: Point,
-    val lowestBeams: Set<Point> = emptySet(),
+    val lowestBeams: Set<Point> = setOf(start),
     val splittersHit: Int = 0,
-    val quantumBeam: Point = start,
 ) {
     private val bottom = splitters.maxBy { it.y }.y
 
-    fun sendQuantumBeam() : Int {
-        return if(bottom == quantumBeam.y) 1
-        else quantumBeam.moveBeam().first.sumOf{ copy(quantumBeam = it).sendQuantumBeam() }
+    fun sendQuantumBeam(): Int {
+        val startQuantumRow = (0..splitters.maxBy { it.x }.x).associate { if (Point(it, 0) != start) it to 0 else start.x to 1 }.toMutableMap()
+        return (0..bottom).fold(startQuantumRow) { previousRow, y ->
+            previousRow.forEach { (x, numberOfBeams) ->
+                if (Point(x, y + 1) !in splitters) previousRow[x] = numberOfBeams
+                else {
+                    previousRow[x - 1] = previousRow.numberOfBeamsLeft(x, y, previousRow[x]!!).also{ println("for ($x-1,$y) set $it")}
+                    previousRow[x + 1] = previousRow.numberOfBeamsRight(x, y, previousRow[x]!!).also{ println("for ($x,$y) set $it")}
+                    previousRow[x] = 0
+                }
+            }
+            previousRow.also { println(it.map { it.value }) }
+        }.values.sumOf { it }
     }
 
-    fun sendBeam() = (0..splitters.maxOf { it.y }).fold(this) { previousManifold, _ ->
+    private fun MutableMap<Int, Int>.numberOfBeamsLeft(x: Int, y: Int, beamsInSpot: Int): Int {
+        return beamsInSpot + if (Point(x - 2, y + 1) in splitters) this[x - 2]!! else 0
+    }
+
+    private fun MutableMap<Int, Int>.numberOfBeamsRight(x: Int, y: Int, beamsInSpot: Int): Int {
+        return beamsInSpot + if (Point(x + 2, y + 1) in splitters) this[x + 2]!! else 0
+    }
+
+
+    fun sendBeam() = (0..bottom).fold(this) { previousManifold, _ ->
         previousManifold.oneClick()
     }
 

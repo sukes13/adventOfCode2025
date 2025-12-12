db19d8084e47dff264e2d771d1aba7d69a310e4f
Jelle Wouters <jelle.wouters@sfpd.fgov.be>
2025-12-12 17:29:14 +0100

    refactoring day 8

--- src/main/kotlin/be/sukes13/aoc2025/day8/JunctionBoxesDay8.kt
+++ src/main/kotlin/be/sukes13/aoc2025/day8/JunctionBoxesDay8.kt
@@ -1,75 +1,47 @@
 package be.sukes13.aoc2025.day8
 
 import be.sukes13.aoc2025.mapLines
-import kotlin.collections.getOrNull
-import kotlin.collections.mapIndexedNotNull
 import kotlin.math.pow
 
-fun part1(input: String, numberOfCircuits: Int) = input.toJunctions()
-    .connectCircuits(numberOfConnections = numberOfCircuits)
+fun part1(input: String, numberOfCircuits: Int) = input.toCircuitSet()
+    .circuitsAfterConnecting(numberOfConnections = numberOfCircuits)
     .map { it.size }
     .sortedByDescending { it }
     .take(3)
     .reduce { a, b -> a * b }
 
-fun part2(input: String) = input.toJunctions()
-    .connectAllCircuits()
-    .let{ it.first.x.toLong() * it.second.x.toLong() }
+fun part2(input: String) = input.toCircuitSet()
+    .lastConnectionToConnectAllJunctions()
+    .let { it.first.x.toLong() * it.second.x.toLong() }
 
-fun Array<Junction>.connectAllCircuits(): Pair<Junction, Junction> {
-    val circuitSet = CircuitSet(this)
-    val junctionPairs = toSortedNormalizedJunctionPairs()
-
-    var indexOfJunctionPair = 0
-    while(circuitSet.circuits().size != 1){
-        circuitSet.connectOneCircuit(junctionPairs, indexOfJunctionPair)
-        indexOfJunctionPair++
-    }
-
-    return junctionPairs[indexOfJunctionPair-1]
-}
-
-private fun Array<Junction>.connectCircuits(numberOfConnections: Int): List<List<Junction>> {
-    val circuitSet = CircuitSet(this)
-    val junctionPairs = toSortedNormalizedJunctionPairs()
-
-    (0 until numberOfConnections).forEach { index ->
-        circuitSet.connectOneCircuit(junctionPairs, index)
-    }
-
-    return circuitSet.circuits()
-}
-
-private fun Array<Junction>.toSortedNormalizedJunctionPairs() =
-    flatMap {
-        mapIndexedNotNull { index, _ ->
-            val secondJunction = getOrNull(index + 1) ?: this[0]
-            if (it != secondJunction) it.normalizePair(secondJunction) else null
-        }
-    }.distinct()
-        .sortedBy { (junction1, junction2) -> junction1.distanceTo(junction2) }
-
-data class Junction(val x: Double, val y: Double, val z: Double) {
-    fun distanceTo(other: Junction) =
-        (x - other.x).pow(2) + (y - other.y).pow(2) + (z - other.z).pow(2)
-
-    fun normalizePair(other: Junction) =
-        if (x + y + z < (other.x + other.y + other.z)) this to other
-        else other to this
-}
-
-class CircuitSet(junctions: Array<Junction>) {
+class CircuitSet(junctions: List<Junction>) {
     private val parent = junctions.associateWith { it }.toMutableMap()
+    private val junctionPairs = toSortedNormalizedJunctionPairs(junctions)
 
-    fun connectOneCircuit(junctionPairs: List<Pair<Junction, Junction>>, index: Int) {
-        junctionPairs[index].let { (junction1, junction2) ->
-            if (!inSameCircuit(junction1, junction2)) {
+    fun lastConnectionToConnectAllJunctions(): Pair<Junction, Junction> {
+        var index = 0
+        while (circuits().size != 1) {
+            junctionPairs[index].connect()
+            index++
+        }
+        return junctionPairs[index - 1]
+    }
+
+    fun circuitsAfterConnecting(numberOfConnections: Int): List<List<Junction>> {
+        (0 until numberOfConnections).forEach { index ->
+            junctionPairs[index].connect()
+        }
+        return circuits()
+    }
+
+    private fun Pair<Junction, Junction>.connect() {
+        let { (junction1, junction2) ->
+            if (!inSameCircuit(junction1, junction2))
                 union(junction1, junction2)
-            }
         }
     }
 
-    fun circuits(): List<List<Junction>> {
+    private fun circuits(): List<List<Junction>> {
         val groups = mutableMapOf<Junction, MutableList<Junction>>()
         parent.keys.forEach { junction ->
             groups.computeIfAbsent(find(junction)) { mutableListOf() }.add(junction)
@@ -77,7 +49,7 @@ class CircuitSet(junctions: Array<Junction>) {
         return groups.values.map { it.toList() }
     }
 
-    private  fun find(junction: Junction): Junction {
+    private fun find(junction: Junction): Junction {
         if (parent[junction] != junction)
             parent[junction] = find(parent[junction]!!)
         return parent[junction]!!
@@ -91,11 +63,31 @@ class CircuitSet(junctions: Array<Junction>) {
 
     private fun inSameCircuit(junction1: Junction, junction2: Junction) =
         find(junction1) == find(junction2)
+
+    private fun toSortedNormalizedJunctionPairs(junctions: List<Junction>) =
+        junctions.flatMap {
+            junctions.mapIndexedNotNull { index, _ ->
+                val secondJunction = junctions.getOrNull(index + 1) ?: junctions[0]
+                if (it != secondJunction) it.normalizedPairWith(secondJunction) else null
+            }
+        }.distinct().sortedBy { (junction1, junction2) -> junction1.distanceTo(junction2) }
+
+}
+
+data class Junction(val x: Double, val y: Double, val z: Double) {
+    fun distanceTo(other: Junction) =
+        (x - other.x).pow(2) + (y - other.y).pow(2) + (z - other.z).pow(2)
+
+    fun normalizedPairWith(other: Junction) =
+        if (x + y + z < (other.x + other.y + other.z)) this to other
+        else other to this
 }
 
 
-private fun String.toJunctions(): Array<Junction> = mapLines { line ->
-    line.split(",")
-        .map { it.toDouble() }
-        .let { Junction(it[0], it[1], it[2]) }
-}.toTypedArray()
+private fun String.toCircuitSet() = CircuitSet(
+    junctions = mapLines { line ->
+        line.split(",")
+            .map { it.toDouble() }
+            .let { Junction(it[0], it[1], it[2]) }
+    }
+)
